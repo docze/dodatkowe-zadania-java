d4dc5b29f9655e93b4bdb1e350f6d5f569ad1165
marekl <m.lunkiewicz@gmail.com>
2022-02-10 10:33:18 +0100

    OldProduct - tests

---
+++ src/test/java/legacyfigher/dietary/newproducts/OldProductTest.java
@@ -0,0 +1,84 @@
+package legacyfigher.dietary.newproducts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.math.BigDecimal;
+
+class OldProductTest {
+
+
+    @Test
+    void shouldBeIncreasedCounter() {
+        // given
+        final OldProduct oldProduct = new OldProduct(new BigDecimal(1), "desc", "longDesc", 0);
+        // when
+        oldProduct.incrementCounter();
+
+        //then
+        Assertions.assertEquals(1, oldProduct.counter);
+    }
+
+    @Test
+    void shouldBeDecreasedCounter() {
+        // given
+        final OldProduct oldProduct = new OldProduct(new BigDecimal(1), "desc", "longDesc", 1);
+        // when
+        oldProduct.decrementCounter();
+
+        //then
+        Assertions.assertEquals(0, oldProduct.counter);
+    }
+
+    @Test
+    void shouldBeThrownExceptionNegativeCounter() {
+        // given
+        final OldProduct oldProduct = new OldProduct(new BigDecimal(1), "desc", "longDesc", 0);
+        // when
+        final Executable decrementCounterAction = oldProduct::decrementCounter;
+
+        //then
+        Assertions.assertThrows(IllegalStateException.class, decrementCounterAction, "Negative counter");
+    }
+
+    @Test
+    void givenAtLeastOneProductAndHasNewPrice() {
+        // given
+        final OldProduct oldProduct = new OldProduct(new BigDecimal(1), "desc", "longDesc", 1);
+        // when
+        final BigDecimal expectedPrice = new BigDecimal(5);
+        oldProduct.changePriceTo(expectedPrice);
+
+        //then
+        Assertions.assertEquals(expectedPrice, oldProduct.price);
+    }
+
+    @Test
+    void shouldReplacedDescription() {
+        // given
+        final OldProduct oldProduct = new OldProduct(new BigDecimal(1), "desc", "longDesc", 1);
+
+        // when
+        oldProduct.replaceCharFromDesc("esc", "");
+
+        // then
+
+        Assertions.assertEquals("longD", oldProduct.longDesc);
+        Assertions.assertEquals("d *** longD", oldProduct.formatDesc());
+    }
+
+
+
+    @Test
+    void shouldReturnEmptyDescription() {
+        // given
+        final OldProduct oldProduct = new OldProduct(new BigDecimal(1), "desc", "longDesc", 1);
+
+        // when
+        oldProduct.replaceCharFromDesc("desc", "");
+
+        // then
+        Assertions.assertEquals("", oldProduct.formatDesc());
+    }
+}
